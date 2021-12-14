e31804e02890624655dc1424b4d3255ccce06213
dragomir003 <dragolekovic@gmail.com>
2021-12-14 22:39:19 +0100

    Initial commit

---
+++ database.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE EkranskaForma;
+
+USE EkranskaForma;
---
+++ ekranska-forma/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data.Sql;
+
+namespace ekranska_forma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            const string connectionString = @"Data Source=DESKTOP-DVS4TKU;Initial Catalog=EkranskaForma;Integrated security=SSPI";
+
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            connection.Open();
+
+            MessageBox.Show(connection.State == ConnectionState.Open ? "Connected" : "Not connected");
+
+            if (connection.State == ConnectionState.Open)
+            {
+                connection.Close();
+            }
+        }
+    }
+}
