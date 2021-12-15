1ff294c24986973e67e016bfee34218912e36408
dragomir003 <dragolekovic@gmail.com>
2021-12-15 10:59:33 +0100

    Estetski detalji

--- ekranska-forma/Form1.Designer.cs
+++ ekranska-forma/Form1.Designer.cs
@@ -49,7 +49,7 @@ namespace ekranska_forma
             // label1
             // 
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
-            this.label1.Location = new System.Drawing.Point(112, 23);
+            this.label1.Location = new System.Drawing.Point(12, 23);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(114, 32);
             this.label1.TabIndex = 0;
@@ -59,27 +59,27 @@ namespace ekranska_forma
             // label2
             // 
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
-            this.label2.Location = new System.Drawing.Point(103, 156);
+            this.label2.Location = new System.Drawing.Point(12, 154);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(123, 32);
+            this.label2.Size = new System.Drawing.Size(226, 32);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Max ucenika";
+            this.label2.Text = "Maksimalan broj ucenika";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label3
             // 
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
-            this.label3.Location = new System.Drawing.Point(112, 84);
+            this.label3.Location = new System.Drawing.Point(12, 84);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(114, 32);
+            this.label3.Size = new System.Drawing.Size(148, 32);
             this.label3.TabIndex = 2;
-            this.label3.Text = "Trajanje";
+            this.label3.Text = "Trajanje(godina)";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label4
             // 
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
-            this.label4.Location = new System.Drawing.Point(94, 228);
+            this.label4.Location = new System.Drawing.Point(12, 226);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(132, 32);
             this.label4.TabIndex = 3;
@@ -97,7 +97,7 @@ namespace ekranska_forma
             // prijemniIspitTb
             // 
             this.prijemniIspitTb.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
-            this.prijemniIspitTb.Location = new System.Drawing.Point(276, 229);
+            this.prijemniIspitTb.Location = new System.Drawing.Point(276, 227);
             this.prijemniIspitTb.Name = "prijemniIspitTb";
             this.prijemniIspitTb.Size = new System.Drawing.Size(267, 29);
             this.prijemniIspitTb.TabIndex = 5;
@@ -105,7 +105,7 @@ namespace ekranska_forma
             // maxUcenikaTb
             // 
             this.maxUcenikaTb.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
-            this.maxUcenikaTb.Location = new System.Drawing.Point(276, 157);
+            this.maxUcenikaTb.Location = new System.Drawing.Point(276, 155);
             this.maxUcenikaTb.Name = "maxUcenikaTb";
             this.maxUcenikaTb.Size = new System.Drawing.Size(267, 29);
             this.maxUcenikaTb.TabIndex = 6;
